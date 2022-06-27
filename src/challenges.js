2f547c1fbe45a97ff495caa606bb891e861f6d3b
Mauricio-OC <murisc0@hotmail.com>
2022-06-27 15:06:51 -0300

    desafio 1

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,12 @@
 // Desafio 1
-function compareTrue() {
-  //
+function compareTrue(parametro1, parametro2) {
+  if(parametro1 === true && parametro2 === true){
+    return true;
+  }else{
+    return false;
+  }
 }
+console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea() {
